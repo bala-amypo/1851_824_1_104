8cb21b0cbaaf3ffa856cd9ddd55077d9b4cd4071
bala-amypo <bala@amypo.in>
2025-12-17 09:11:54 +0000

    User commit

--- src/main/java/com/example/demo/model/EligibilityCheckRecord.java
+++ src/main/java/com/example/demo/model/EligibilityCheckRecord.java
@@ -1,9 +1,71 @@
-package.com.example.demo.model;
-public class EligiblityCheckerRecord{
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "eligibility_check")
+public class EligibilityCheckRecord {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String deviceCode;
-    private String DeviceType;
-    private String model;
-    private Integer maxAllowedPerEmployee;
-    private Boolean active;
+
+    @ManyToOne
+    private EmployeeProfile employee;
+
+    @ManyToOne
+    private DeviceCatalogItem device;
+
+    private Boolean eligible;
+    private String reason;
+    private LocalDateTime checkedAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public EmployeeProfile getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(EmployeeProfile employee) {
+        this.employee = employee;
+    }
+
+    public DeviceCatalogItem getDevice() {
+        return device;
+    }
+
+    public void setDevice(DeviceCatalogItem device) {
+        this.device = device;
+    }
+
+    public Boolean getEligible() {
+        return eligible;
+    }
+
+    public void setEligible(Boolean eligible) {
+        this.eligible = eligible;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public LocalDateTime getCheckedAt() {
+        return checkedAt;
+    }
+
+    public void setCheckedAt(LocalDateTime checkedAt) {
+        this.checkedAt = checkedAt;
+    }
 }

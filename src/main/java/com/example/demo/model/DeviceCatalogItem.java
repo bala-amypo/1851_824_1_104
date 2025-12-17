77c918d2e9fe5882e8c74c7757054f9ca23b9764
bala-amypo <bala@amypo.in>
2025-12-17 08:48:52 +0000

    User commit

--- src/main/java/com/example/demo/model/DeviceCatalogItem.java
+++ src/main/java/com/example/demo/model/DeviceCatalogItem.java
@@ -1,1 +1,11 @@
-package.
+package.com.example.demo.model;
+public class DeviceCatalogItem{
+    private Long id;
+    private String EmployeeId;
+    private String fullName;
+    private String email;
+    private String department;
+    private String jobRole;
+    private Boolean active;
+    private LocalDate
+}

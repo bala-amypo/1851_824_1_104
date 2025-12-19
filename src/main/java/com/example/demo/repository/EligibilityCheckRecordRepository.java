98dba648d5f8039b6b77fb910c8b5d663353dbe0
bala-amypo <bala@amypo.in>
2025-12-19 07:52:42 +0000

    User commit

--- src/main/java/com/example/demo/repository/EligibilityCheckRecordRepository.java
+++ src/main/java/com/example/demo/repository/EligibilityCheckRecordRepository.java
@@ -1,17 +1,8 @@
-package com.example.demo.service;
+package com.example.demo.repository;
 
 import com.example.demo.model.EligibilityCheckRecord;
-import com.example.demo.repository.EligibilityCheckRecordRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-@Service
-public class EligibilityCheckService {
-
-    @Autowired
-    private EligibilityCheckRecordRepository repository;
-
-    public EligibilityCheckRecord save(EligibilityCheckRecord record) {
-        return repository.save(record);
-    }
+public interface EligibilityCheckRecordRepository
+        extends JpaRepository<EligibilityCheckRecord, Long> {
 }
--- src/main/java/com/example/demo/service/EligibilityCheckService.java
+++ src/main/java/com/example/demo/service/EligibilityCheckService.java
@@ -1,35 +1,17 @@
 package com.example.demo.service;
 
 import com.example.demo.model.EligibilityCheckRecord;
-import com.example.demo.model.EmployeeProfile;
-import com.example.demo.model.DeviceCatalogItem;
 import com.example.demo.repository.EligibilityCheckRecordRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import java.util.List;
-import java.util.Optional;
 
 @Service
 public class EligibilityCheckService {
 
-    private final EligibilityCheckRecordRepository repository;
+    @Autowired
+    private EligibilityCheckRecordRepository repository;
 
-    public EligibilityCheckService(EligibilityCheckRecordRepository repository) {
-        this.repository = repository;
-    }
-
-    public List<EligibilityCheckRecord> getAllChecks() {
-        return repository.findAll();
-    }
-
-    public Optional<EligibilityCheckRecord> getCheckById(Long id) {
-        return repository.findById(id);
-    }
-
-    public EligibilityCheckRecord saveCheck(EligibilityCheckRecord record) {
+    public EligibilityCheckRecord save(EligibilityCheckRecord record) {
         return repository.save(record);
-    }
-
-    public void deleteCheck(Long id) {
-        repository.deleteById(id);
     }
 }

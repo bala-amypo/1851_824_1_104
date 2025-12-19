06729dccd1f940bf85e0640a9672ea23a77c84fe
bala-amypo <bala@amypo.in>
2025-12-19 23:35:43 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/IssuedDeviceRecordServiceImpl.java
@@ -0,0 +1,9 @@
+package com.example.demo.service.impl;
+
+import org.springframework.stereotype.Service;
+import com.example.demo.service.IssuedDeviceRecordService;
+
+@Service
+public class IssuedDeviceRecordServiceImpl
+        implements IssuedDeviceRecordService {
+}
---
+++ src/main/java/com/example/demo/service/impl/PolicyRuleServiceImpl.java
@@ -0,0 +1,9 @@
+package com.example.demo.service.impl;
+
+import org.springframework.stereotype.Service;
+import com.example.demo.service.PolicyRuleService;
+
+@Service
+public class PolicyRuleServiceImpl
+        implements PolicyRuleService {
+}

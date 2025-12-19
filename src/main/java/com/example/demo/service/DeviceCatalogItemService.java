ea682ea0e85eb5808310eb76dbd8188a34d25610
bala-amypo <bala@amypo.in>
2025-12-19 23:26:45 +0000

    User commit

--- src/main/java/com/example/demo/service/DeviceCatalogItemService.java
+++ src/main/java/com/example/demo/service/DeviceCatalogItemService.java
@@ -1,23 +1,7 @@
 package com.example.demo.service;
 
-import com.example.demo.model.DeviceCatalogItem;
-import com.example.demo.repository.DeviceCatalogItemRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class DeviceCatalogItemService {
-
-    @Autowired
-    private DeviceCatalogItemRepository repository;
-
-    public List<DeviceCatalogItem> getAllDevices() {
-        return repository.findAll();
-    }
-
-    public DeviceCatalogItem save(DeviceCatalogItem item) {
-        return repository.save(item);
-    }
 }
--- src/main/java/com/example/demo/service/EligibilityCheckService.java
+++ src/main/java/com/example/demo/service/EligibilityCheckService.java
@@ -1,17 +1,7 @@
 package com.example.demo.service;
 
-import com.example.demo.model.EligibilityCheckRecord;
-import com.example.demo.repository.EligibilityCheckRecordRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EligibilityCheckService {
-
-    @Autowired
-    private EligibilityCheckRecordRepository repository;
-
-    public EligibilityCheckRecord save(EligibilityCheckRecord record) {
-        return repository.save(record);
-    }
 }

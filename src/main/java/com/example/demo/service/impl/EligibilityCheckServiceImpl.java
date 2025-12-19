c56402590d15bc89d5e4d5ad7b7ce4222037eb2e
bala-amypo <bala@amypo.in>
2025-12-19 23:32:41 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/EligibilityCheckServiceImpl.java
@@ -0,0 +1,9 @@
+package com.example.demo.service.impl;
+
+import org.springframework.stereotype.Service;
+import com.example.demo.service.EligibilityCheckService;
+
+@Service
+public class EligibilityCheckServiceImpl
+        implements EligibilityCheckService {
+}
---
+++ src/main/java/com/example/demo/service/impl/EmployeeProfileServiceImpl.java
@@ -0,0 +1,9 @@
+package com.example.demo.service.impl;
+
+import org.springframework.stereotype.Service;
+import com.example.demo.service.EmployeeProfileService;
+
+@Service
+public class EmployeeProfileServiceImpl
+        implements EmployeeProfileService {
+}

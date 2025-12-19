c82838c54b8b09923a5304861586803d2b05ab75
bala-amypo <bala@amypo.in>
2025-12-19 08:52:42 +0000

    User commit

--- src/main/java/com/example/demo/dto/AuthResponse.java
+++ src/main/java/com/example/demo/dto/AuthResponse.java
@@ -2,18 +2,39 @@ package com.example.demo.dto;
 
 public class AuthResponse {
     private String token;
-    private String message;
+    private String username;
+    private String role;
 
-    public AuthResponse() {}
-
-    public AuthResponse(String token, String message) {
-        this.token = token;
-        this.message = message;
+    public AuthResponse() {
     }
 
-    public String getToken() { return token; }
-    public void setToken(String token) { this.token = token; }
+    public AuthResponse(String token, String username, String role) {
+        this.token = token;
+        this.username = username;
+        this.role = role;
+    }
 
-    public String getMessage() { return message; }
-    public void setMessage(String message) { this.message = message; }
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
--- src/main/java/com/example/demo/dto/RegisterRequest.java
+++ src/main/java/com/example/demo/dto/RegisterRequest.java
@@ -1,24 +1,40 @@
 package com.example.demo.dto;
 
 public class RegisterRequest {
+    private Long id;
     private String username;
     private String email;
-    private String password;
 
-    public RegisterRequest() {}
-
-    public RegisterRequest(String username, String email, String password) {
-        this.username = username;
-        this.email = email;
-        this.password = password;
+    public RegisterRequest() {
     }
 
-    public String getUsername() { return username; }
-    public void setUsername(String username) { this.username = username; }
+    public RegisterReuest(Long id, String username, String email) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+    }
 
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
+    public Long getId() {
+        return id;
+    }
 
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }

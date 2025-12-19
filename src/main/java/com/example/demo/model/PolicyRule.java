2138d363aa3e9d547e6f2d756083dd905c4a634d
bala-amypo <bala@amypo.in>
2025-12-19 07:43:41 +0000

    User commit

--- src/main/java/com/example/demo/model/PolicyRule.java
+++ src/main/java/com/example/demo/model/PolicyRule.java
@@ -1,6 +1,6 @@
 package com.example.demo.model;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.model;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;

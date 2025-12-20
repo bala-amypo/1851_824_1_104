14ed8eccf0f604ee7eb166c6c9a02d69fbfcb5b8
bala-amypo <bala@amypo.in>
2025-12-20 00:38:42 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/DeviceCatalogItemServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/DeviceCatalogItemServiceImpl.java
@@ -1,17 +1,21 @@
 package com.example.demo.service.impl;
 
 import com.example.demo.model.DeviceCatalogItem;
+import com.example.demo.repository.DeviceCatalogItemRepository;
 import com.example.demo.service.DeviceCatalogItemService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public interface DeviceCatalogItemServiceImpl
-        implements DeviceCatalogItemService {
+public class DeviceCatalogItemServiceImpl implements DeviceCatalogItemService {
+
+    @Autowired
+    private DeviceCatalogItemRepository repository;
 
     @Override
     public List<DeviceCatalogItem> getAllItems() {
-        return repository.findAll();;
+        return repository.findAll();
     }
 }

0a82809aa9a629995462b821c44b4c19917b674f
bala-amypo <bala@amypo.in>
2025-12-20 01:49:25 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/PolicyRuleServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/PolicyRuleServiceImpl.java
@@ -1,10 +1,21 @@
-// File: src/main/java/com/example/demo/service/impl/PolicyRuleServiceImpl.java
+// File: src/main/java/com/example/demo/service/impl/PolicyRuleServiceDummyImpl.java
 package com.example.demo.service.impl;
 
 import com.example.demo.model.PolicyRule;
+import com.example.demo.service.PolicyRuleService;
+import org.springframework.stereotype.Service;
 import java.util.List;
 
-public interface PolicyRuleServiceImpl {
-    List<PolicyRule> getAllPolicies();
-    PolicyRule addPolicy(PolicyRule policy);
+@Service
+public class PolicyRuleServiceDummyImpl implements PolicyRuleService {
+
+    @Override
+    public List<PolicyRule> getAllPolicies() {
+        return null; // dummy return
+    }
+
+    @Override
+    public PolicyRule addPolicy(PolicyRule policy) {
+        return null; // dummy return
+    }
 }
